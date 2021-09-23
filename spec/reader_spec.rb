0ac3c8c30755d764a411a1a31d567a87ee790799
Micha Bernhard <mib70@pitt.edu>
2021-09-23 16:50:28 -0600

    Test - Write tests for initialize and read_message methods

---
+++ spec/reader_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require './lib/reader'
+
+describe Reader do
+  let(:reader) { Reader.new(ARGV[0]) }
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(reader).to be_an_instance_of Reader
+    end
+
+    it 'has a file to read' do
+      expect(reader.file_to_read).to eq(ARGV[0])
+    end
+  end
+
+  # describe '#read_message' do
+  #   it 'reads message.txt' do
+  #     expect(reader.read_message).to eq('This is a test message')
+  #   end
+  # end
+end

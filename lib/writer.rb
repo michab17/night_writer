e258e1ab2064d660589c8f7295eca3e69e5d4135
Micha Bernhard <mib70@pitt.edu>
2021-09-24 17:32:56 -0600

    Feat - Make the new file be a translated version of the original file

--- lib/writer.rb
+++ lib/writer.rb
@@ -1,14 +1,43 @@
 require './lib/reader'
+require './lib/dictionary'
 
 class Writer
   attr_reader :file_to_write
 
   def initialize(argv)
     @file_to_write = argv.to_s
-    @reader = Reader.new(ARGV[0])
+    # @reader = Reader.new(ARGV[0])
+    @dictionary = Dictionary.new('message.txt', {
+        'a' => ['0.', '..', '..'],
+        'b' => ['0.', '0.', '..'],
+        'c' => ['00', '..', '..'],
+        'd' => ['00', '.0', '..'],
+        'e' => ['0.', '.0', '..'],
+        'f' => ['00', '0.', '..'],
+        'g' => ['00', '00', '..'],
+        'h' => ['0.', '00', '..'],
+        'i' => ['.0', '0.', '..'],
+        'j' => ['.0', '00', '..'],
+        'k' => ['0.', '..', '0.'],
+        'l' => ['0.', '0.', '0.'],
+        'm' => ['00', '..', '0.'],
+        'n' => ['00', '.0', '0.'],
+        'o' => ['0.', '.0', '0.'],
+        'p' => ['00', '0.', '0.'],
+        'q' => ['00', '00', '0.'],
+        'r' => ['0.', '00', '0.'],
+        's' => ['.0', '0.', '0.'],
+        't' => ['.0', '00', '0.'],
+        'u' => ['0.', '..', '00'],
+        'v' => ['0.', '0.', '00'],
+        'w' => ['.0', '00', '.0'],
+        'x' => ['00', '..', '00'],
+        'y' => ['00', '.0', '00'],
+        'z' => ['0.', '.0', '00']
+        })
   end
 
   def create_file
-    File.open(@file_to_write, "w") { |file| file.write @reader.get_file_contets }
+    File.open(@file_to_write, "w") { |file| file.write @dictionary.print_first_line + "\n" + @dictionary.print_second_line + "\n" + @dictionary.print_third_line}
   end
 end

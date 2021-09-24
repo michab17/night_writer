227f7bbb29d53d77d4152fc701c5428776f81f69
Micha Bernhard <mib70@pitt.edu>
2021-09-23 18:03:54 -0600

    Test - Add test for get_char_count

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require './lib/reader'
 
 describe Reader do
-  let(:reader) { Reader.new(ARGV[0]) }
+  let(:reader) { Reader.new('message.txt') }
 
   describe '#initialize' do
     it 'exists' do
@@ -10,14 +10,20 @@ describe Reader do
     end
 
     it 'has a file to read' do
-      expect(reader.file_to_read).to eq(ARGV[0])
+      expect(reader.file_to_read).to eq('message.txt')
     end
   end
 
-  # describe '#get_file_contets' do
-  #   it 'gets the contents of message.txt' do
-  #     file_to_read = double("./lib/message.txt")
-  #     expect(reader.get_file_contets).to eq('This is a test message')
-  #   end
-  # end
+  describe '#get_file_contets' do
+    it 'gets the contents of message.txt' do
+      file_to_read = double("./lib/message.txt")
+      expect(reader.get_file_contets).to eq('This is a test message.')
+    end
+  end
+
+  describe 'get_char_count' do
+    it 'gets the count of characters in the file that is read' do
+      expect(reader.get_char_count).to eq(23)
+    end
+  end
 end

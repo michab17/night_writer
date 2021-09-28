8c7327d4729ed480828b8352b64cca60f8e49e4d
Micha Bernhard <mib70@pitt.edu>
2021-09-27 23:54:20 -0600

    Refactor - Change spelling of contents

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -5,6 +5,6 @@ require './lib/night_reader_writer'
 reader = Reader.new(ARGV[0])
 writer = NightReaderWriter.new(ARGV[1])
 
-reader.get_file_contets
+reader.get_file_contents
 writer.create_english_file
 puts "Created '#{ARGV[1]}' containing #{reader.get_char_count / 6} characters"

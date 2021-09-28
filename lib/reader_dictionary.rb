54b2a329f67af1b8fbdb098726ca09781ba08e1f
Micha Bernhard <mib70@pitt.edu>
2021-09-28 00:11:33 -0600

    Refactor - Delete unneeded comments

--- lib/reader_dictionary.rb
+++ lib/reader_dictionary.rb
@@ -8,7 +8,6 @@ class ReaderDictionary
     @library = library
     @reader = Reader.new('braille.txt')
     @code = @reader.get_file_contents
-    # require 'pry'; binding.pry
   end
 
   def one_letter(letter)
@@ -23,7 +22,6 @@ class ReaderDictionary
     new_string = ''
     word = word.gsub!("\n", '')
     word = word.scan(/.{1,240}/)
-    # test = word.scan(/.{1,2}/)
     word.each do |string|
       test1 = string.scan(/.{1,2}/)
       test = test1.each_slice(test1.length / 3).to_a
@@ -35,5 +33,3 @@ class ReaderDictionary
     new_string
   end
 end
-# this program can translate everything you put in here as long as it is not symbols or numbers or anything weird
-# ".00.00..000.0..0...00..0.0..0.000.00...00.0.0....00.000..0.0000..0...0..000.00.0\n0000.0...0.0.00...00000.0....0.00..0..00.000....0..0...0000....00...0....0.0.000\n.0..00....0...0...0.....0...0.0.0.00...00.0.0...0.0.0...0...0...0.........0.0.0."

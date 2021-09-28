dcaa23b408d053c9f27ede75e8a0780f241bcd10
Micha Bernhard <mib70@pitt.edu>
2021-09-27 23:53:34 -0600

    Refactor - Correct spelling of contents

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -4,6 +4,6 @@ require './lib/writer'
 reader = Reader.new(ARGV[0])
 writer = Writer.new(ARGV[1])
 
-reader.get_file_contets
+reader.get_file_contents
 writer.create_file
 puts "Created 'braille.txt' containing #{reader.get_char_count} characters"

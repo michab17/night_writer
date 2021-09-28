86062f8c5825ab3c77d579ff30fe97fdfb694988
Micha Bernhard <mib70@pitt.edu>
2021-09-27 23:59:33 -0600

    Test - Make tests pass

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -17,13 +17,15 @@ describe Reader do
   describe '#get_file_contets' do
     it 'gets the contents of message.txt' do
       file_to_read = double("./lib/message.txt")
-      expect(reader.get_file_contets).to eq('This is a test message.')
+
+      expected = "this program can translate everything you put in here as long as it is not symbols or numbers or anything weird"
+      expect(reader.get_file_contents).to eq(expected)
     end
   end
 
   describe 'get_char_count' do
     it 'gets the count of characters in the file that is read' do
-      expect(reader.get_char_count).to eq(23)
+      expect(reader.get_char_count).to eq(111)
     end
   end
 end
